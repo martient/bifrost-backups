958130b89140d993a65b3bac8bcf00fec7a06f80
Arnaud Leherpeur <arnaud.leherpeur@taultek.com>
2024-05-15 00:25:12 +0200

    fix (config) config file override solve and improved errors

--- pkg/setup/register_storage.go
+++ pkg/setup/register_storage.go
@@ -29,7 +29,7 @@ func InteractiveRegisterStorage() {
 	// defer file.Close()
 
 	if _, err := tea.NewProgram(interactives.LocalStorageInitialModel()).Run(); err != nil {
-		fmt.Printf("could not start program: %s\n", err)
+		utils.LogError("Could not start program: %s\n", "Register datbase", err)
 		os.Exit(1)
 	}
 
@@ -43,8 +43,8 @@ func InteractiveRegisterStorage() {
 func RegisterLocalStorage(path string) (*localstorage.LocalStorageRequirements, error) {
 	requirements := &localstorage.LocalStorageRequirements{}
 	if len(path) <= 0 {
-		utils.LogError("Path can't be empty", "Register postgresql database", nil)
-		return nil, fmt.Errorf("username can't be empty")
+		utils.LogError("Path can't be empty", "Register local storage", nil)
+		return nil, fmt.Errorf("path can't be empty")
 	}
 	requirements.FolderPath = path
 	return requirements, nil
@@ -70,6 +70,11 @@ func RegisterStorage(storageType StorageType, name string, storage interface{}) 
 	}
 	configMutex.Lock()
 	defer configMutex.Unlock()
+
+	exist := checkIfAlreadyExist(name, config.Storages)
+	if exist {
+		return fmt.Errorf("storage name already used")
+	}
 
 	newStorage := &Storage{
 		Type: storageType,
