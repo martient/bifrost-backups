ea7140298d0c89c1ac8935a4ccf84b906f4fa29b
Arnaud Leherpeur <arnaud.leherpeur@taultek.com>
2024-05-20 09:46:05 +0200

    feat (backup) pg_dump exec work, need to investigate about the PGPASSWORD option and save the backup

--- pkg/postgresql/backup.go
+++ pkg/postgresql/backup.go
@@ -1,61 +1,135 @@
+// package postgresql
+
+// import (
+// 	"fmt"
+// 	"os/exec"
+// 	"strings"
+
+// 	"github.com/martient/golang-utils/utils"
+// )
+
+// func RunBackup(database PostgresqlRequirements) error {
+// 	if (database == PostgresqlRequirements{}) {
+// 		return fmt.Errorf("database can't be empty")
+// 	}
+
+// 	var builderPASSWORD strings.Builder
+// 	var builderPATH strings.Builder
+// 	var builderHOSTNAME strings.Builder
+// 	var builderPORT strings.Builder
+// 	var builderUSER strings.Builder
+// 	var builderNAME strings.Builder
+// 	if database.Password != "" {
+// 		builderPASSWORD.WriteString("PGPASSWORD=")
+// 		builderPASSWORD.WriteString(database.Password)
+// 		builderPASSWORD.WriteString(" ")
+// 	}
+// 	builderPATH.WriteString("pg_dump")
+
+// 	if database.Hostname != "" {
+// 		builderHOSTNAME.WriteString("-h ")
+// 		builderHOSTNAME.WriteString(database.Hostname)
+// 		builderHOSTNAME.WriteString(" ")
+// 	}
+// 	if database.Port != "" {
+// 		builderPORT.WriteString("-p ")
+// 		builderPORT.WriteString(database.Port)
+// 		builderPORT.WriteString(" ")
+// 	}
+// 	if database.User != "" {
+// 		builderUSER.WriteString("-U ")
+// 		builderUSER.WriteString(database.User)
+// 		builderUSER.WriteString(" ")
+// 	}
+// 	if database.Name != "" {
+// 		builderNAME.WriteString(database.Name)
+// 	}
+
+// 	_, err := exec.LookPath("pg_dump")
+// 	if err != nil {
+// 		utils.LogError("pg_dump must be installed, please install it", "POSTGRESQL", err)
+// 		return err
+// 	}
+
+// utils.LogInfo("CLI cmd: %s %s %s %s %s %s", "POSTGRESQL", builderPASSWORD.String(), builderPATH.String(), builderHOSTNAME.String(), builderPORT.String(), builderUSER.String(), builderNAME.String())
+// output, err := exec.Command(builderPASSWORD.String(), builderPATH.String(), builderHOSTNAME.String(), builderPORT.String(), builderUSER.String(), builderNAME.String()).CombinedOutput()
+// if err != nil {
 package postgresql
 
 import (
+	"bytes"
 	"fmt"
 	"os/exec"
-	"strings"
 
 	"github.com/martient/golang-utils/utils"
 )
 
+const pgDumpCommand = "pg_dump"
+
 func RunBackup(database PostgresqlRequirements) error {
-	if (database == PostgresqlRequirements{}) {
-		return fmt.Errorf("database can't be empty")
-	}
-
-	var builderPATH strings.Builder
-	var builderPARAMS strings.Builder
-
-	// if database.Password != "" {
-	// 	builderPATH.WriteString("PGPASSWORD=")
-	// 	builderPATH.WriteString(database.Password)
-	// 	builderPATH.WriteString(" ")
-	// }
-	builderPATH.WriteString("pg_dump")
-
-	if database.Hostname != "" {
-		builderPARAMS.WriteString("-h ")
-		builderPARAMS.WriteString(database.Hostname)
-		builderPARAMS.WriteString(" ")
-	}
-	if database.Port != "" {
-		builderPARAMS.WriteString("-p ")
-		builderPARAMS.WriteString(database.Port)
-		builderPARAMS.WriteString(" ")
-	}
-	if database.User != "" {
-		builderPARAMS.WriteString("-U ")
-		builderPARAMS.WriteString(database.User)
-		builderPARAMS.WriteString(" ")
-	}
-	if database.Name != "" {
-		builderPARAMS.WriteString(database.Name)
-	}
-
-	_, err := exec.LookPath("pg_dump")
-	if err != nil {
-		utils.LogError("pg_dump must be installed, please install it", "POSTGRESQL", err)
+	if err := validateRequirements(database); err != nil {
 		return err
 	}
 
-	utils.LogInfo("CLI cmd: %s %s", "POSTGRESQL", builderPATH.String(), builderPARAMS.String())
-	output, err := exec.Command(builderPATH.String(), builderPARAMS.String()).Output()
+	pgDumpPath, err := exec.LookPath(pgDumpCommand)
 	if err != nil {
-		fmt.Println(err)
-		fmt.Println(output)
-		utils.LogError("Something went wrong during the execution", "POSTGRESQL", err)
-		return err
+		return fmt.Errorf("pg_dump command not found: %w", err)
 	}
-	fmt.Println(output)
+
+	args := buildCommandArgs(database)
+	cmd := exec.Command(pgDumpPath, args...)
+	utils.LogInfo(cmd.String(), "POSTGRESQL")
+
+	var buffer bytes.Buffer
+	cmd.Stdout = &buffer
+
+	err = cmd.Run()
+	if err != nil {
+		utils.LogError("Failed to backup database '%s'", "POSTGRESQL", err)
+		return fmt.Errorf("backup failed: %w", err)
+	}
 	return nil
 }
+
+func validateRequirements(database PostgresqlRequirements) error {
+	if database == (PostgresqlRequirements{}) {
+		return fmt.Errorf("database requirements cannot be empty")
+	}
+
+	// Add additional validation checks for port, hostname, etc.
+
+	return nil
+}
+
+func buildCommandArgs(database PostgresqlRequirements) []string {
+	var args []string
+
+	// if database.Password != "" {
+	// 	args = append(args, "PGPASSWORD="+database.Password)
+	// }
+
+	if database.Hostname != "" {
+		args = append(args, "-h", database.Hostname)
+	}
+
+	if database.Port != "" {
+		args = append(args, "-p", database.Port)
+	}
+
+	if database.User != "" {
+		args = append(args, "-U", database.User)
+	}
+
+	args = append(args, database.Name)
+
+	return args
+}
+
+// 		fmt.Println(err)
+// 		fmt.Println(string(output))
+// 		utils.LogError("Something went wrong during the execution", "POSTGRESQL", err)
+// 		return err
+// 	}
+// 	fmt.Println(output)
+// 	return nil
+// }
