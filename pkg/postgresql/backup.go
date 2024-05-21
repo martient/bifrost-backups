bcf0e3251cade461eb2c3a082f9688fcaebf332c
Arnaud Leherpeur <arnaud.leherpeur@taultek.com>
2024-05-21 12:12:48 +0200

    fix (postgresql): added password capacity with env var

--- pkg/postgresql/backup.go
+++ pkg/postgresql/backup.go
@@ -1,64 +1,9 @@
-// package postgresql
-
-// import (
-// 	"fmt"
-// 	"os/exec"
-// 	"strings"
-
-// 	"github.com/martient/golang-utils/utils"
-// )
-
-// func RunBackup(database PostgresqlRequirements) error {
-// 	if (database == PostgresqlRequirements{}) {
-// 		return fmt.Errorf("database can't be empty")
-// 	}
-
-// 	var builderPASSWORD strings.Builder
-// 	var builderPATH strings.Builder
-// 	var builderHOSTNAME strings.Builder
-// 	var builderPORT strings.Builder
-// 	var builderUSER strings.Builder
-// 	var builderNAME strings.Builder
-// 	if database.Password != "" {
-// 		builderPASSWORD.WriteString("PGPASSWORD=")
-// 		builderPASSWORD.WriteString(database.Password)
-// 		builderPASSWORD.WriteString(" ")
-// 	}
-// 	builderPATH.WriteString("pg_dump")
-
-// 	if database.Hostname != "" {
-// 		builderHOSTNAME.WriteString("-h ")
-// 		builderHOSTNAME.WriteString(database.Hostname)
-// 		builderHOSTNAME.WriteString(" ")
-// 	}
-// 	if database.Port != "" {
-// 		builderPORT.WriteString("-p ")
-// 		builderPORT.WriteString(database.Port)
-// 		builderPORT.WriteString(" ")
-// 	}
-// 	if database.User != "" {
-// 		builderUSER.WriteString("-U ")
-// 		builderUSER.WriteString(database.User)
-// 		builderUSER.WriteString(" ")
-// 	}
-// 	if database.Name != "" {
-// 		builderNAME.WriteString(database.Name)
-// 	}
-
-// 	_, err := exec.LookPath("pg_dump")
-// 	if err != nil {
-// 		utils.LogError("pg_dump must be installed, please install it", "POSTGRESQL", err)
-// 		return err
-// 	}
-
-// utils.LogInfo("CLI cmd: %s %s %s %s %s %s", "POSTGRESQL", builderPASSWORD.String(), builderPATH.String(), builderHOSTNAME.String(), builderPORT.String(), builderUSER.String(), builderNAME.String())
-// output, err := exec.Command(builderPASSWORD.String(), builderPATH.String(), builderHOSTNAME.String(), builderPORT.String(), builderUSER.String(), builderNAME.String()).CombinedOutput()
-// if err != nil {
 package postgresql
 
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 
 	"github.com/martient/golang-utils/utils"
@@ -78,7 +23,11 @@ func RunBackup(database PostgresqlRequirements) (*bytes.Buffer, error) {
 
 	args := buildCommandArgs(database)
 	cmd := exec.Command(pgDumpPath, args...)
-	utils.LogInfo(cmd.String(), "POSTGRESQL")
+
+	if database.Password != "" {
+		cmd.Env = os.Environ()
+		cmd.Env = append(cmd.Env, "PGPASSWORD="+database.Password)
+	}
 
 	var buffer bytes.Buffer
 	cmd.Stdout = &buffer
@@ -103,10 +52,6 @@ func validateRequirements(database PostgresqlRequirements) error {
 
 func buildCommandArgs(database PostgresqlRequirements) []string {
 	var args []string
-
-	// if database.Password != "" {
-	// 	args = append(args, "PGPASSWORD="+database.Password)
-	// }
 
 	if database.Hostname != "" {
 		args = append(args, "-h", database.Hostname)
